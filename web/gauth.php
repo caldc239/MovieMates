85859de923b3cb46d21051dfcc986c5bb88e8915
caldc239 <55357449+caldc239@users.noreply.github.com>
2021-07-06 21:45:03 -0400

    pretty print user info

--- web/gauth.php
+++ web/gauth.php
@@ -16,7 +16,9 @@ if(isset($_GET['code'])) {
 
 		// Get user information
 		$user_info = GetUserProfileInfo($access_token);
-		echo ($user_info);
+		echo('<p>');
+		print_r ($user_info);
+		echo('</p>');
 	}
 	catch(Exception $e) {
 		echo $e->getMessage();
