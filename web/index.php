ac3c435bdb6b963ef877ca1893aeceb8f3e71292
caldc239 <55357449+caldc239@users.noreply.github.com>
2021-07-06 21:15:57 -0400

    +https

--- web/index.php
+++ web/index.php
@@ -13,11 +13,11 @@
     <title>MovieMates</title>
 
     <!-- CSS -->
-    <link rel="stylesheet" href="http://code.jquery.com/mobile/1.4.5/jquery.mobile-1.4.5.min.css" />
+    <link rel="stylesheet" href="https://code.jquery.com/mobile/1.4.5/jquery.mobile-1.4.5.min.css" />
 
     <!-- JS -->
-    <script src="http://code.jquery.com/jquery-1.11.1.min.js"></script>
-    <script src="http://code.jquery.com/mobile/1.4.5/jquery.mobile-1.4.5.min.js"></script>
+    <script src="https://code.jquery.com/jquery-1.11.1.min.js"></script>
+    <script src="https://code.jquery.com/mobile/1.4.5/jquery.mobile-1.4.5.min.js"></script>
 
     <!-- PHP -->
     <?php
