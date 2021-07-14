5486ae4c31d98cad68489be56368a2eb02c2e4f8
caldc239 <55357449+caldc239@users.noreply.github.com>
2021-07-14 09:50:55 -0400

    Reordered page divs - navbar on homeNotLogin + login JS

--- public/scripts/auth.js
+++ public/scripts/auth.js
@@ -3,25 +3,47 @@ const signupForm = $('#signupForm');
 signupForm.submit((e) => {
 	e.preventDefault();
 
-	// user info
+	// User info
 	const email = $('#signupEmail').val();
 	const password = $('#signupPassword').val();
 
-	// sign up the user
+	// Sign up the user
 	auth.createUserWithEmailAndPassword(email, password).then(cred => {
 		console.log(cred.user)
 	});
 
+	// Clear form & redirect user to logged-in homepage
 	$('#signupForm').trigger("reset");
 	$(':mobile-pagecontainer').pagecontainer('change', '#homePageLogin');
 
 });
 
-//Logout
+// Logout
 const logout = $('#logout');
 logout.click((e) => {
 	e.preventDefault();
 	auth.signOut().then(() => {
 		console.log('user signed out');
 	});
+	// Redirect user to not-logged-in homepage 
+	$(':mobile-pagecontainer').pagecontainer('change', '#homePageNotLogin');
 });
+
+// Login
+const loginForm = $('#loginForm');
+loginForm.submit((e) => {
+	e.preventDefault();
+
+	// User info
+	const email = $('#loginEmail').val();
+	const password = $('#loginPassword').val();
+
+	// Login the user
+	auth.signInWithEmailAndPassword(email, password).then(cred => {
+		console.log(cred.user)
+	});
+
+	// Clear form & redirect user to logged-in homepage
+	$('#loginForm').trigger("reset");
+	$(':mobile-pagecontainer').pagecontainer('change', '#homePageLogin');
+})
